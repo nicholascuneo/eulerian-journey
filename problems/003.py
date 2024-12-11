7ba1d910d5d2b21e481f86e7496cdfe714afb3c9
Nick C <69825418+nicholascuneo@users.noreply.github.com>
2024-12-11 00:31:36 -0800

    Complete function to find largest prime factor

--- problems/003.py
+++ problems/003.py
@@ -7,19 +7,34 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
 """
 
-def solve(limit):
-    primes = []
+def max_prime_factor(number):
+    """
+    Finds the largest prime factor of a given number.
+    """
+    
+    prime_candidates = [] 
+    max_limit = int(number ** 0.5) # No need to check factors beyond sqrt of number
 
-    for num in range(2, limit):
+    # Generate list of prime candidates up to sqrt of number
+    for candidate in range(2, max_limit + 1): # Check integers from 2 up to sqrt of number
         is_prime = True
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
+        for divisor in range(2, int(candidate ** 0.5) + 1): # Test divisors up to sqrt of candidate
+            if candidate % divisor == 0: # Candidate not prime if divisible
                 is_prime = False
-                break
+                break # Exit since candidate is not prime
         if is_prime:
-                primes.append(num)
+                prime_candidates.append(candidate) # Add prime to list
 
-    return primes
+    prime_factors = []
 
+    # Check which primes are factors of given number
+    for prime in prime_candidates:
+        if number % prime == 0: # If divsible, is a prime factor of given number
+             prime_factors.append(prime)
+
+    # Find and return largest prime factor of given number
+    largest_prime_factor = max(prime_factors)
+    return largest_prime_factor
+     
 if __name__ == "__main__":
-    print(solve(50))
+    print(max_prime_factor(600851475143))
