2c248bb3e09e20d73984036caed6c39d996de107
Nick C <69825418+nicholascuneo@users.noreply.github.com>
2024-12-22 09:09:36 -0800

    Find product of triplet such that a + b + c = 100
    Loop over possible values of n and m to generate triplets using
    Euclid's Formula.  Once a triplet with a sum of 100 is found,
    return the product of the triplet.

--- problems/009.py
+++ problems/009.py
@@ -12,25 +12,51 @@ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 """
 
+import numpy as np
 
-def euclid_formula(n, m):
-    a = (m * m) - (n * n)
-    b = 2 * (m * n)
-    c = (m * m) + (n * n)
 
-    triplet = [a, b, c]
+def generate_triplet(n, m):
+    """
+    Generates a Pythagorean triplet [a, b, c] using Euclid's Formula.
+
+    Parameters:
+    n (int): Smaller number for formula
+    m (int): Larger number for formula (m > n > 0)
+
+    Return:
+    A list containing the triplet [a, b, c]
+
+    """
+    side_a = (m * m) - (n * n)  # Calculate first side of triplet
+    side_b = 2 * (m * n)  # Calculate second side of triplet
+    side_c = (m * m) + (n * n)  # Calculate hypotenuse side of triplet
+
+    triplet = [side_a, side_b, side_c]
     return triplet
 
 
-def solve():
-    triplets = []
-    for n in range(501):
-        for m in range(501):
-            if m > n > 0:
-                triplets.append(euclid_formula(n, m))
+def find_triplet_product():
+    """
+    Finds product of the Pythagorean triplet [a, b, c] such that a + b + c = 1000.
 
-    return triplets[0:10]
+    Uses Euclid's formula to generate triplets and iterates over possible values of
+    n and m (ensuring m > n > 0). If a triplet with a sum of 1000 is found, the product
+    of the triplet is calculated and returned.
+
+    Return:
+    The product of the triplet [a, b, c] as an integer if its sum equals 1000.
+    """
+    for smaller_value in range(501):  # Loop over n from 0 to 500
+        for larger_value in range(501):  # Loop over m from 0 to 500
+            if (
+                larger_value > smaller_value > 0
+            ):  # Ensure valid input for Euclid's formula
+                triplet = generate_triplet(
+                    smaller_value, larger_value
+                )  # Generate triplet
+                if sum(triplet) == 1000:  # Check if sum of triplet = 1000
+                    return np.prod(triplet)  # Calculate and return product of triplet
 
 
 if __name__ == "__main__":
-    print(solve())
+    print(find_triplet_product())
