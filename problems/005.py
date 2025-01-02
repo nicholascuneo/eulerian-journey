46c1ba969a85e40b9292c9b7b8d2795680e87180
Nick C <69825418+nicholascuneo@users.noreply.github.com>
2025-01-02 00:29:14 -0800

    Avoid equality comparisons to True

--- problems/005.py
+++ problems/005.py
@@ -9,10 +9,11 @@ What is the smallest number that is evenly divisible by all of the numbers
 from 1 to 20?
 """
 
+
 def is_divisible_by_all(number, divisors):
     """
-    Checking if a given number is evenly divisible by all divisors in a 
-    givenlist without any remainder.  
+    Checking if a given number is evenly divisible by all divisors in a
+    givenlist without any remainder.
 
     args: Given number, list of divisors
 
@@ -25,24 +26,25 @@ def is_divisible_by_all(number, divisors):
             return False
     return True
 
+
 def solve():
     """
     Find the smallest number divisible by all numbers from 1 to 20.
 
-    Starts from 20 and checks each number until one is found that all divisors 
+    Starts from 20 and checks each number until one is found that all divisors
     can divide evenly.
 
     Return smallest number that meets the condition.
     """
-    start = 20 # Begin at largest divisor for efficiency
-    divisors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-    
+    start = 20  # Begin at largest divisor for efficiency
+    divisors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
     while True:
         # Check if current number is divisible by all divisors
-        if is_divisible_by_all(start, divisors) == True:
-            return start # Return number once found
-        start += 1 # Increment to check next number
+        if is_divisible_by_all(start, divisors):
+            return start  # Return number once found
+        start += 1  # Increment to check next number
+
 
 if __name__ == "__main__":
     print(solve())
